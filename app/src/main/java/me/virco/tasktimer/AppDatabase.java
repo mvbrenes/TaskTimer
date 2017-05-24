f99cf99e356746b29b0f35892e9a671098482edc
Virco <dev.virco@gmail.com>
2017-05-24 14:46:11 -0400

    master: Created Database Singleton class, started on ContentProvider

---
+++ app/src/main/java/me/virco/tasktimer/AppDatabase.java
@@ -0,0 +1,65 @@
+package me.virco.tasktimer;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by mvbrenes on 5/24/17.
+ *
+ * Basic database class for the application.
+ *
+ * The only class that should use this is AppProvider.
+ */
+
+class AppDatabase extends SQLiteOpenHelper {
+    private static final String TAG = "AppDatabase";
+
+    public static final String DATABASE_NAME = "TaskTimer.db";
+    public static final int DATABASE_VERSION = 1;
+
+    // Implement AppDatabase as a Singleton
+    private static AppDatabase instance = null;
+
+    private AppDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d(TAG, "AppDatabase: constructor");
+    }
+
+    static AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            Log.d(TAG, "getInstance: creating new instance");
+            instance = new AppDatabase(context);
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(TAG, "onCreate: starts");
+        String sSQL;    // Use a string variable to facilitate logging
+        sSQL = "CREATE TABLE "
+                + TaskContract.TABLE_NAME + " ("
+                + TaskContract.Columns._ID + " INTEGER PRIMARY KEY NOT NULL, "
+                + TaskContract.Columns.TASKS_NAME + " TEXT NOT NULL, "
+                + TaskContract.Columns.TASKS_DESCRIPTION + " TEXT, "
+                + TaskContract.Columns.TASKS_SORT_ORDER + " INTEGER);";
+        Log.d(TAG, sSQL);
+        db.execSQL(sSQL);
+        Log.d(TAG, "onCreate: ends");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d(TAG, "onUpgrade: starts");
+        switch (oldVersion) {
+            case 1:
+                // upgrade logic from version 1
+                break;
+            default:
+                throw new IllegalStateException("onUpgrade() with unknown newVersion: " + newVersion);
+        }
+        Log.d(TAG, "onUpgrade: ends");
+    }
+}
---
+++ app/src/main/java/me/virco/tasktimer/AppProvider.java
@@ -0,0 +1,56 @@
+package me.virco.tasktimer;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by mvbrenes on 5/24/17.
+ *
+ * Provider for the TaskTimer app. This is the only class that knows about {@link AppDatabase}
+ */
+
+public class AppProvider extends ContentProvider {
+    private static final String TAG = "AppProvider";
+
+    private AppDatabase mOpenHelper;
+
+    static final String CONTENT_AUTHORITY = "me.virco.tasktimer.provider";
+    public static final Uri CONTENT_AUTHORITY_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
