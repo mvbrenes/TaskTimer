5e251a67471c480764eb3a2efc0059db79a764f2
Virco <dev.virco@gmail.com>
2017-06-08 16:49:05 -0400

    master: Started AppDialog

---
+++ app/src/main/java/me/virco/tasktimer/AppDialog.java
@@ -0,0 +1,111 @@
+package me.virco.tasktimer;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+
+/**
+ * Created by mvbrenes on 6/8/17.
+ */
+
+public class AppDialog extends DialogFragment {
+    private static final String TAG = "AppDialog";
+
+    public static final String DIALOG_ID = "id";
+    public static final String DIALOG_MESSAGE = "message";
+    public static final String DIALOG_POSITIVE_RID = "positive_rid";
+    public static final String DIALOG_NEGATIVE_RID = "negative_rid";
+
+    /**
+     * Tha dialogs' callback interface to notify of user selected results (deletion confirmed, etc.)
+     */
+    interface DialogEvents {
+        void onPositiveDialogResult(int dialogId, Bundle args);
+        void onNegativeDialogResult(int dialogId, Bundle args);
+        void onDialogCancelled(int dialogId, Bundle args);
+    }
+
+    private DialogEvents mDialogEvents;
+
+    @Override
+    public void onAttach(Context context) {
+        Log.d(TAG, "onAttach: context is " + context.toString());
+        super.onAttach(context);
+        try {
+            mDialogEvents = (DialogEvents) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement DialogEvents interface");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "onDetach: starts");
+        super.onDetach();
+        // Reset the active callbacks interface, becasue we don't have an activity any longer
+        mDialogEvents = null;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateDialog: starts");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final Bundle arguments = getArguments();
+        final int dialogId;
+        String messageString;
+        int positiveStringId;
+        int negativeStringId;
+
+        if (arguments != null) {
+            dialogId = arguments.getInt(DIALOG_ID);
+            messageString = arguments.getString(DIALOG_MESSAGE);
+            if (dialogId == 0 || messageString == null) {
+                throw new IllegalArgumentException("DIALOG_ID and/or DIALOG_MESSAGE not present in the bundle");
+            }
+
+            positiveStringId = arguments.getInt(DIALOG_POSITIVE_RID);
+            if (positiveStringId == 0) {
+                positiveStringId = R.string.ok;
+            }
+            negativeStringId = arguments.getInt(DIALOG_NEGATIVE_RID);
+            if (negativeStringId == 0) {
+                negativeStringId = R.string.cancel;
+            }
+        }
+
+        builder.setMessage(messageString)
+                .setPositiveButton(positiveStringId, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // callback positive result method
+                mDialogEvents.onPositiveDialogResult(dialogId, arguments);
+            }
+        })
+                .setNegativeButton(negativeStringId, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // callback negative result method
+                mDialogEvents.onNegativeDialogResult(dialogId, arguments);
+            }
+        });
+
+        return super.onCreateDialog(savedInstanceState);
+    }
+
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        Log.d(TAG, "onCancel: ");
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        Log.d(TAG, "onDismiss: ");
+    }
+}
